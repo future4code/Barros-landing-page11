9e0f481f442fb24294dbec19999d4fad1227057a
emmanuel <emmanuel-jnr@hotmail.com>
2022-06-17 20:39:57 -0300

    entregaProjeto

--- index.js
+++ index.js
@@ -1,11 +1,3 @@
-function mobileDropDown(){
-    if(document.getElementById("menuMob").style.display == "flex"){
-        document.querySelector(".open").style.display = "flex"
-        document.querySelector(".close").style.display = "none"
-        document.getElementById("menuMob").style.display = "none"
-    } else{
-        document.getElementById("menuMob").style.display = "flex"
-        document.querySelector(".open").style.display = "none"
-        document.querySelector(".close").style.display = "flex"
-    }
+function chamar(){
+    alert("chamandoooooo")
 }
